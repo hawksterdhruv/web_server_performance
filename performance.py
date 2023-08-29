c739be7fd02ef46a004e2dd5cb55f3f08593d8f4
dhruv.shah <hawksterdhruv@gmail.com>
2023-08-29 17:55:18 +0530

    threading requests

--- performance.py
+++ performance.py
@@ -1,6 +1,23 @@
 # performance.py
 
 import requests
+import threading
 
-requests.get('http://localhost:5000/wait')
-requests.get('http://localhost:5000/wait')
+def make_request():
+    requests.get('http://localhost:5000/wait')
+
+
+
+
+def run_requests(make_request):
+    t = threading.Thread(target=make_request)
+    t.start()
+    return t
+
+if __name__ == "__main__":
+
+    # [make_request() for i in range(4)]    
+    threads = [run_requests(make_request) for i in range(10)]
+    
+    for thread in threads:
+        thread.join()
